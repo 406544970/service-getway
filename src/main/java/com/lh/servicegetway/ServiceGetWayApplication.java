896f3fcaba8724e66a506c47c49e0c23cd1e968f
Administrator <406544970@qq.com>
2019-10-14 11:26:54 +0800

    2019.10.14.2

--- src/main/java/com/lh/servicegetway/ServiceGetWayApplication.java
+++ src/main/java/com/lh/servicegetway/ServiceGetWayApplication.java
@@ -1,19 +1,22 @@
 package com.lh.servicegetway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
 public class ServiceGetWayApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(ServiceGetwayApplication.class, args);
-        new SpringApplicationBuilder(ServiceGetWayApplication.class).web(WebApplicationType.SERVLET).run(args);
+//        SpringApplication.run(ServiceGetWayApplication.class, args);
+        new SpringApplicationBuilder(ServiceGetWayApplication.class).web(WebApplicationType.REACTIVE).run(args);
         System.out.println("http://localhost:2005/swagger-ui.html");
 
     }
